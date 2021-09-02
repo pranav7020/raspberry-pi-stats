30a22bc22e824a37d7d704fcbca478712be8772c
pranav <pranav7020@gmail.com>
2021-09-02 08:13:43 +0530

    fix err handler

--- public/app.js
+++ public/app.js
@@ -1,120 +1,25 @@
-// let systemInfo = {
-//     hostname: 'nebula',
-//     model: 'Raspberry Pi 3 Model B Plus Rev 1.3',
-//     os: 'Raspbian GNU/Linux 9 (stretch)',
-//     kernel: '4.14.98-v7+',
-//     processor: 'ARMv7 Processor rev 4 (v7l)',
-//     arch: 'armv7l',
-//     hardware: 'BCM2835',
-// };
-
-// let metrics = {
-//     temperature: '54.2',
-//     voltage: '1.35',
-//     serverTime: {
-//         H: '12',
-//         M: '45'
-//     },
-//     uptime: {
-//         days: '0',
-//         hours: '1',
-//         minutes: '17',
-//         seconds: '31'
-//     }
-// };
-
-// let memory = {
-//     ram: {
-//         total: '949448',
-//         used: '374136',
-//         free: '446468',
-//         shared: '6112',
-//         cache: '128844',
-//         available: '539412'
-//     },
-//     swap: {
-//         total: '102396',
-//         used: '7168',
-//         free: '95228'
-//     }
-// };
-
-// let diskUsage = [
-//     {
-//         filesystem: '/dev/root',
-//         type: 'ext4',
-//         size: 14843,
-//         used: 5111,
-//         available: 9060,
-//         use: '37%',
-//         mounted: '/'
-//     },
-//     {
-//         filesystem: '/dev/mmcblk0p1',
-//         type: 'vfat',
-//         size: 43,
-//         used: 22,
-//         available: 21,
-//         use: '51%',
-//         mounted: '/boot'
-//     }
-// ];
-
-// let network = {
-//     etho: {
-//         mac: 'b8:27:eb:89:36:0f',
-//         ip: '-'
-//     },
-//     wlan0: {
-//         mac: 'b8:27:eb:dc:63:5a',
-//         ip: '192.168.43.141/24'
-//     }
-// };
-
-// let ps = `PID CMD                         %MEM %CPU\n1461 /home/pi/.vscode-server/bin 13.1  3.5\n1460 /home/pi/.vscode-server/bin  9.3  0.6\n1423 /home/pi/.vscode-server/bin  9.0  3.5\n1483 /home/pi/.vscode-server/bin  5.4  0.1\n1122 /home/pi/.vscode-server/bin  4.8  0.4\n1430 /home/pi/.vscode-server/bin  3.4  0.0\n487 /usr/lib/xorg/Xorg :0 -seat  3.4  0.0\n1352 /home/pi/.vscode-server/bin  3.4  0.2\n5782 node commands.js             2.8 44.0\n`
-
-// let sshClients = [
-//     {
-//         ip: '192.168.43.82',
-//         port: '55268'
-//     },
-//     {
-//         ip: '192.168.43.82',
-//         port: '55268'
-//     }
-// ];
-
-// const ws_hostname = window.location.hostname;
-const ws_hostname = '192.168.43.141';
-const ws_port = 8080;
-
-const ws_URL = `ws://${ws_hostname}:${ws_port}`;
-
-// create websocket connection
-const socket = new WebSocket(ws_URL);
-
-socket.onopen = (open) => {
-    console.log(`WebSocket open : `)
-    socket.send(JSON.stringify({ ping: 'hi iam connected' }))
-}
-
-socket.onmessage = (msg) => {
-    console.log(`WebSocket message : `)
-    // socket.send(JSON.stringify({ ping: 'pong' }))
-    console.log(JSON.parse(msg.data));
-    metrics = JSON.parse(msg.data);
-}
-
-socket.onclose = close => {
-    console.log(`WebSocket close : `)
-}
-
-socket.onerror = error => {
-    console.log(`WebSocket error: `)
-    console.log(error)
-}
-
 (async () => {
+    const ws_hostname = window.location.hostname;
+    const ws_port = 8080;
+    const ws_URL = `ws://${ws_hostname}:${ws_port}`;
+
+    // create websocket connection
+    const socket = new WebSocket(ws_URL);
+
+    socket.onopen = (open) => {
+        document.getElementById('live-status').classList.add('on');
+    }
+
+    socket.onclose = close => {
+        document.getElementById('live-status').classList.remove('on');
+        document.getElementById('error').innerText = 'Websocket connection closed!';
+    }
+
+    socket.onerror = error => {
+        document.getElementById('live-status').classList.remove('on');
+        document.getElementById('error').innerText = 'Websocket connection failed!';
+    }
+
     socket.onmessage = (msg) => {
         console.log(`WebSocket message : `)
         console.log(JSON.parse(msg.data));
@@ -123,9 +28,9 @@ socket.onerror = error => {
         let memory = data.memory;
         let ps = data.process;
 
-        metricsUI(metrics);
-        memoryStatusUI(memory);
-        processUI(ps);
+        metrics && metricsUI(metrics);
+        memory && memoryStatusUI(memory);
+        ps && processUI(ps);
     }
 
     // ------------------ General system information ------------------
@@ -249,17 +154,16 @@ socket.onerror = error => {
     fetch(`${window.location.origin}/info`)
         .then(response => response.json())
         .then(data => {
-            console.log('Success:', data);
-            metricsUI(data.metrics);
-            memoryStatusUI(data.memory);
-            processUI(data.process);
-            systemInfoUI(data.systemInfo);
-            diskUsageUI(data.diskUsage);
-            networkStatusUI(data.network);
-            sshClientUI(data.ssh)
+            data.metrics && metricsUI(data.metrics);
+            data.memory && memoryStatusUI(data.memory);
+            data.process && processUI(data.process);
+            data.systemInfo && systemInfoUI(data.systemInfo);
+            data.network && networkStatusUI(data.network);
+            data.diskUsage && data.diskUsage.length > 0 && diskUsageUI(data.diskUsage);
+            data.diskUsage && data.diskUsage.length > 0 ? sshClientUI(data.ssh) : sshClientUI([]);
         })
         .catch((error) => {
-            console.error('Error:', error);
+            document.getElementById('error').innerText = 'Failed to getting server info!';
         });
 })()
 
